f3cc04add52e70912e8ecd5b9a62d0a032f374fb
ankit-gupta6791 <91ankitgupta@gmail.com>
2025-03-25 08:15:25 +0530

    2. Canvas to Box With DraweBehind and DrawWithContent

--- app/src/main/java/in/androaid/composeinternals/CanvasModifiers.kt
+++ app/src/main/java/in/androaid/composeinternals/CanvasModifiers.kt
@@ -1,7 +1,13 @@
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -9,8 +15,17 @@ import androidx.compose.ui.tooling.preview.Preview
 @Composable
 fun CanvasModifiersDemo(modifier: Modifier = Modifier) {
 
-    Canvas(modifier = Modifier.fillMaxSize()) {
-        drawCircle(color = Color.Red )
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Green)
+        .drawWithContent {
+            drawCircle(color = Color.Red)
+            drawContent()
+        }
+       ,
+        contentAlignment = Alignment.Center
+    ){
+        Text("Hello world")
     }
 
 }
